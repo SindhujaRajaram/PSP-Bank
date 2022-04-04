6b15946214add1c6ec4e5f19982bfbeca0506948
SindhujaRajaram <sindhuit94@gmail.com>
2022-04-04 21:05:51 +0100

    Banking

---
+++ create_account.py
@@ -0,0 +1,29 @@
+import random
+import sqlite3
+connection = sqlite3.connect('accdetails.db')
+cursor = connection.cursor()
+
+
+def acc_details():
+    command1 = " CREATE TABLE IF NOT EXISTS " \
+               "account(Account_no INTEGER PRIMARY KEY, Cust_name TEXT, bal FLOAT)"
+    cursor.execute(command1)
+    name = input("Enter your name: ")
+    ac_no = random.randint(400000, 5000000)
+    print("Your Account number:", ac_no)
+    bal = int(input(" Enter the amount you want to deposit: "))
+    add = "INSERT INTO account (Account_no,Cust_name, bal) VALUES(?, ?, ?)"
+    cursor.execute(add, (ac_no, name, bal))
+    print("YOUR ACCOUNT IS CREATED\n")
+    connection.commit()
+
+
+def show_details():
+    no = int(input("Enter the Account number: "))
+    cursor.execute("SELECT * FROM account")
+
+    for no in cursor.fetchone():
+        print(no)
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from create_account import *
+
+print("WELCOME TO PSP BANK\n"
+      "1. CREATE ACCOUNT\n"
+      "2. VIEW ACCOUNT DETAILS\n"
+      "3. WITHDRAW\n"
+      "4. DEPOSIT")
+choice = int(input("ENTER YOUR CHOICE: "))
+if choice == 1:
+    acc_details()
+
+elif choice == 2:
+
+    show_details()
